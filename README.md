cc784df774a6d568ded4cf21a482c365aad2c216
qjupinu <calinstefantamas@gmail.com>
2025-01-04 18:21:43 +0200

    update20250104

--- README.md
+++ README.md
@@ -1,14 +1,31 @@
-# fdum
+# FDUM
 File&amp;DiskUsageMonitor
 
-Update Jan 3 2025
+Update Jan 4 2025
 
-Se ruleaza script.sh
+## Descriere
+
+FDUM are ca scop monitorizarea atat a structurii de fisiere si directoare relevante pentru utilizator, cat si a utilizarii spatiului pe disc, prin interpretarea sesiunii de lucru (terminal) a utilizatorului.
+
+Programul este capabil sa genereze si sa parseze automat fisiere typescript (create prin comanda script), pe care le stocheaza sub forma unor snapshot-uri. Utilizatorul poate folosi fdum pentru a compara aceste snapshot-uri, si a vizualiza diferentele intr-un format usor de inteles.
+
+Pe langa interpretarea in format citibil a output-ului comenzilor ls -l si df generate in typescript, programul va compara si continutul fisierelor care au fost modificate intre cele 2 snapshot-uri, daca utilizatorul doreste acest lucru.
+Interfata este prietenoasa; utilizatorul se poate intoarce mereu la meniul principal, iar instructiunile si output-ul sunt usor de inteles.
+
+- Fisierele si directoarele care au fost adaugate, sterse sau modificate vor fi monitorizate de fdum in directorul myfiles
+- Snapshot-urile (fisierele typescript neparsate) vor fi salvate in directorul snaps
+- Rezultatul interpretatii va fi afisat in stdout
+
+## Usage
+
+Se executa fdum.sh (fara argumente suplimentare)
 Pot fi selectate 4 optiuni:
 1. generarea unui snapshot cu rezultatul sub forma de fisier typescript
 2. compararea a doua snapshot-uri generate la momente diferite de timp
 3. compararea unui snapshot cu starea actuala a directorului
 4. exit din program
 
-Mai sunt de facut:
--final debugging si eventual output mai frumos
+## Recomandari
+
+- Este foarte indicat ca fisierele si directoarele de monitorizat (cele din directorul myfiles) sa foloseasca doar caractere alfanumerice, fara spatii sau caractere speciale.
+- Este indicat ca fiecare fisier din myfiles sa aiba separator de linie la final.
--- fdum.sh
+++ fdum.sh
@@ -3,20 +3,22 @@
 NC='\033[0m'
 RED='\033[0;31m'
 GREEN='\033[0;32m'
-LIGHTGREEN='\033[1;32m'
+LGREEN='\033[1;32m'
 YELLOW='\033[1;33m'
 CYAN='\033[0;36m'
 LCYAN='\033[1;36m'
+PURPLE='\033[0;35m'
 
 USERDIR='myfiles'
 TSDIR='snaps'
 TEMPDIR='temp'
-REPORTSDIR='reports'
 
-mkdir -p $TSDIR
+mkdir -p "$TSDIR"
+rm -rf "$TEMPDIR"
 
 # Functie generare typescript
 generate_typescript() {
+
 	TSNAME=$1
 	TS="$TSDIR/$TSNAME"
 	#TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
@@ -31,10 +33,10 @@ generate_typescript() {
 	COMMANDS="${COMMANDS}exit\n"
 	echo -e "$COMMANDS" > script_commands
 	script "$TS" < script_commands > /dev/null 2>&1
-	echo -e "\n${GREEN}Snapshot generat cu succes!\nLocatie fisier typescript: $TS${NC}\n"
-	rm script_commands
-	read -p "Apasa [ENTER] pentru a continua"
 	clear
+	echo -e "${GREEN}Snapshot generat cu succes!\nLocatie fisier typescript: $TS${NC}\n"
+	rm script_commands
+	#read -p "Apasa [ENTER] pentru a continua"
 
 }
 
@@ -65,21 +67,21 @@ parse_typescript() {
 		# Extrage filename fara prefix
 		split($0, arr, "/")
 		outfile = arr[length(arr)]
-
-		# Sterge \r$ din filename
 		sub(/\r$/, "", outfile)
 		outfile = "'$TSEFD'" "/" outfile
+		system("touch " outfile)
 		next
 	}
 	/exit/ || /cat '$USERDIR'\// {
 		# Opreste scrierea cand ajunge la urmatoarea comanda (cat sau exit)
-		outfile = ""
-		next
+		if (outfile) close(outfile)
+    	outfile = ""
+    	next
 	}
 	{
 		# Actualizeaza fisierul curent
 		if (outfile) {
-			sub(/\r$/, "")  # Remove carriage returns from content
+			sub(/\r$/, "")
 			print > outfile
 		}
 	}
@@ -88,6 +90,10 @@ parse_typescript() {
 
 # Compara 2 snapshot-uri
 compare() {
+
+	anyLsChanges=0
+	anyDfChanges=0
+
 	# ordine cronologica
 	date1=$(date -r "$TSDIR/$1" "+%s")
 	date2=$(date -r "$TSDIR/$2" "+%s")
@@ -102,9 +108,9 @@ compare() {
 	TSPDIR2="${TS2}_PARSED"
 	parse_typescript "$TS1" "$TSPDIR1"
 	parse_typescript "$TS2" "$TSPDIR2"
+
 	TSEFD1="$TSPDIR1/files"
 	TSEFD2="$TSPDIR2/files"
-
 	
 	ls_diff="$TEMPDIR/ls_diff"
 	df_diff="$TEMPDIR/df_diff"
@@ -112,8 +118,8 @@ compare() {
 
 	echo -e "${LCYAN}============ REZULTAT ============\n${NC}Comparatie snapshot ${YELLOW}$1 ${NC}VS ${YELLOW}$2${NC}\n"
 	
-	date1=$(date -r $TS1 "+%m-%d-%Y %H:%M:%S")
-	date2=$(date -r $TS2 "+%m-%d-%Y %H:%M:%S")
+	date1=$(date -r $TS1 "+%Y-%m-%d %H:%M:%S")
+	date2=$(date -r $TS2 "+%Y-%m-%d %H:%M:%S")
 	echo -e "Typescript ${YELLOW}$TS1${NC}: generat $date1"
 	echo -e "Typescript ${YELLOW}$TS2${NC}: generat $date2\n"
 	
@@ -126,10 +132,11 @@ compare() {
 	# in checked vom salva fisierele/directoarele parcurse
 	checked_files="$TEMPDIR/checked_files"
 	checked_dirs="$TEMPDIR/checked_dirs"
-
+	
 	# fisiere / directoare deja existente: apar o singura data in fisier, iar linia incepe cu ' '
 	unmodified_files="$TEMPDIR/unmodified_files"
 	unmodified_dirs="$TEMPDIR/unmodified_directories"
+
 
 	# fisiere / directoare deja existente, dar modificate: apar de mai multe ori in fisier
 	modified_files="$TEMPDIR/modified_files"
@@ -149,7 +156,7 @@ compare() {
 		char1=${line:1:1}
 		char3=${line:3:1}
 		# Evitam linii de tipul "+++ *" sau "--- *"
-		if [[ $char3 != ' ' ]]; then
+		if [[ $line != +++' '* && $line != ---' '* ]]; then
 
 			# Stergem posibile culori / caractere speciale generate de terminal in typescript-ul original
 			color_removal="$TEMPDIR/color_removal"
@@ -174,7 +181,7 @@ compare() {
 			elif [[ $char0 == [+-] ]]; then
 				case $char1 in
 					'-')
-					exists=$(grep "$entity" $checked_files > /dev/null 2>&1)
+					exists=$(grep -w "$entity" $checked_files > /dev/null 2>&1)
 					if [ $? -ne 0 ]; then 
 						echo "$entity" >> "$checked_files"
 					else
@@ -182,7 +189,7 @@ compare() {
 					fi
 					;;
 					'd')
-					exists=$(grep "$entity" $checked_dirs > /dev/null 2>&1)
+					exists=$(grep -w "$entity" $checked_dirs > /dev/null 2>&1)
 					if [ $? -ne 0 ]; then 
 						echo "$entity" >> "$checked_dirs"
 					else
@@ -192,7 +199,7 @@ compare() {
 				esac
 			fi
 		fi
-	done < "$ls_diff"
+	done < "$ls_diff" > /dev/null 2>&1
 
 	# Partea a 2-a; parcurgem din nou rezultatul ls_diff linie cu linie
 	# A ramas sa stabilim care fisiere au fost adaugate si care au fost sterse
@@ -209,13 +216,13 @@ compare() {
 				'+')
 				case $char1 in
 					'-')
-					exists=$(grep "$entity" $modified_files > /dev/null 2>&1)
+					exists=$(grep -w "$entity" $modified_files > /dev/null 2>&1)
 					if [ $? -ne 0 ]; then 
 						echo "$entity" >> "$added_files"
 					fi
 					;;
 					'd')
-					exists=$(grep "$entity" $modified_dirs > /dev/null 2>&1)
+					exists=$(grep -w "$entity" $modified_dirs > /dev/null 2>&1)
 					if [ $? -ne 0 ]; then 
 						echo "$entity" >> "$added_dirs"
 					fi
@@ -225,13 +232,13 @@ compare() {
 				'-')
 				case $char1 in
 					'-')
-					exists=$(grep "$entity" $modified_files > /dev/null 2>&1)
+					exists=$(grep -w "$entity" $modified_files > /dev/null 2>&1)
 					if [ $? -ne 0 ]; then 
 						echo "$entity" >> "$removed_files"
 					fi
 					;;
 					'd')
-					exists=$(grep "$entity" $modified_dirs > /dev/null 2>&1)
+					exists=$(grep -w "$entity" $modified_dirs > /dev/null 2>&1)
 					if [ $? -ne 0 ]; then 
 						echo "$entity" >> "$removed_dirs"
 					fi
@@ -240,44 +247,60 @@ compare() {
 				;;
 			esac
 		fi
-	done < "$ls_diff"
+	done < "$ls_diff" > /dev/null 2>&1
 	
-	echo -e "${CYAN}Structura directoare / fisiere:${NC}"
+	echo -e "${LCYAN}Structura directoare / fisiere:${NC}"
+
+	cat "$modified_files" > /dev/null 2>&1
+	if [ $? -eq 0 ]; then
+		echo "Fisiere modificate:"
+		cat "$modified_files"
+		echo ""
+		anyLsChanges=1
+	fi
 
 	cat "$added_files" > /dev/null 2>&1
 	if [ $? -eq 0 ]; then
 		echo "Fisiere adaugate:"
 		cat "$added_files"
+		echo ""
+		anyLsChanges=1
 	fi
 
 	cat "$removed_files" > /dev/null 2>&1
 	if [ $? -eq 0 ]; then
 		echo "Fisiere sterse:"
 		cat "$removed_files"
-	fi
-
-	cat "$modified_files" > /dev/null 2>&1
-	if [ $? -eq 0 ]; then
-		echo "Fisiere modificate:"
-		cat "$modified_files"
-	fi
-
-	cat "$added_dirs" > /dev/null 2>&1
-	if [ $? -eq 0 ]; then
-		echo "Directoare adaugate:"
-		cat "$added_dirs"
-	fi
-
-	cat "$removed_dirs" > /dev/null 2>&1
-	if [ $? -eq 0 ]; then
-		echo "Directoare sterse:"
-		cat "$removed_dirs"
+		echo ""
+		anyLsChanges=1
 	fi
 
 	cat "$modified_dirs" > /dev/null 2>&1
 	if [ $? -eq 0 ]; then
 		echo "Directoare modificate:"
 		cat "$modified_dirs"
+		echo ""
+		anyLsChanges=1
+	fi
+
+	cat "$added_dirs" > /dev/null 2>&1
+	if [ $? -eq 0 ]; then
+		echo "Directoare adaugate:"
+		cat "$added_dirs"
+		echo ""
+		anyLsChanges=1
+	fi
+
+	cat "$removed_dirs" > /dev/null 2>&1
+	if [ $? -eq 0 ]; then
+		echo "Directoare sterse:"
+		cat "$removed_dirs"
+		echo ""
+		anyLsChanges=1
+	fi
+
+	if [[ $anyLsChanges == 0 ]]; then
+		echo -e "${GREEN}Structura nemodificata!${NC}\n"
 	fi
 
 
@@ -289,15 +312,14 @@ compare() {
 	
 	checked_mountpoints="$TEMPDIR/checked_mountpoints"
 	checked_size_used="$TEMPDIR/checked_size_used"
-	unmodified_mountpoints="$TEMPDIR/unmodified_files"
-	modified_mountpoints="$TEMPDIR/modified_files" # aici ar trebui sa calculam diferenta in spatiul utilizat
+	unmodified_mountpoints="$TEMPDIR/unmodified_mountpoints"
+	modified_mountpoints="$TEMPDIR/modified_mountpoints" # aici ar trebui sa calculam diferenta in spatiul utilizat
 	modified_size_used="$TEMPDIR/modified_size_used" #delta
-	added_mountpoints="$TEMPDIR/added_dirs"
-	removed_mountpoints="$TEMPDIR/removed_files"
+	added_mountpoints="$TEMPDIR/added_mountpoints"
+	removed_mountpoints="$TEMPDIR/removed_mountpoints"
 
-	echo -e "\n${CYAN}Spatiu pe disc:${NC}"
-
-	#prima data parcurgem fisierul df_diff pentru a 
+	#prima data parcurgem fisierul df_diff pentru a pentru a determina mountpoint-urile modificate
+	# + spatiul modificat
 	while IFS= read -r line; do
 		char0=${line:0:1}
 		char1=${line:1:1}
@@ -325,77 +347,163 @@ compare() {
 						fi
 					done < "$checked_mountpoints" > /dev/null 2>&1
 					if [ $found -eq 0 ]; then 
-						echo $string >> $checked_mountpoints
+						echo $string >> "$checked_mountpoints"
 					else
 						echo $mountpoint >> "$modified_mountpoints"
 						# facem delta
 						while read -r checked_line; do
 							if [[ $checked_line == *' '$mountpoint ]]; then
 								usedOriginal=$(echo "$checked_line" | awk '{print $1}')
-								#echo $used
-								#echo $usedOriginal
-								delta=$(($used - $usedOriginal))
-								echo $mountpoint
-								echo "Mountpoint-ul s-a modificat cu $delta KB"
+								sizeDifference=$(($used - $usedOriginal))
+								delta=${sizeDifference#-}
+								echo -e ${PURPLE}$mountpoint${NC} >> "$modified_size_used"
+								case $char0 in
+									'+')
+									if [ $sizeDifference -lt 0 ]; then
+										echo -e "Spatiul utilizat s-a micsorat cu ${PURPLE}${delta}KB${NC}." >> "$modified_size_used"
+									else
+										echo -e "Spatiul utilizat s-a marit cu ${PURPLE}${delta}KB${NC}." >> "$modified_size_used"
+									fi 
+									;;
+									-)
+									if [ $sizeDifference -le 0 ]; then
+										echo -e "Spatiul utilizat s-a marit cu ${PURPLE}${delta}KB${NC}." >> "$modified_size_used"
+									else
+										echo -e "Spatiul utilizat s-a micsorat cu ${PURPLE}${delta}KB${NC}." >> "$modified_size_used"
+									fi 
+									;;
+								esac
+								echo "" >> "$modified_size_used" > /dev/null 2>&1
 							fi
-						done < "$checked_mountpoints" 2> /dev/null
+						done < "$checked_mountpoints" #2> /dev/null
 
 					fi	
 				fi
 			fi
 		fi
-	done < "$df_diff"
+	done < "$df_diff" > /dev/null 2>&1
 
-	# Partea a 2-a; parcurgem din nou rezultatul ls_diff linie cu linie
-	# A ramas sa stabilim care fisiere au fost adaugate si care au fost sterse
+
+	# Partea a 2-a pentru df compare
+	# Verificam daca au fost adaugate sau sterse mountpoint-uri
 	while IFS= read -r line; do
 		char0=${line:0:1}
 		char1=${line:1:1}
-		char3=${line:3:1}
-		if [[ $char3 != ' ' ]]; then
-			color_removal="$TEMPDIR/color_removal"
-			echo ${line##*' '} > "$color_removal"
-			entity=$(sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2};?)?)?[mGK]//g" "$color_removal")
-
-			case $char0 in
-				'+')
-				case $char1 in
-					'-')
-					exists=$(grep "$entity" $modified_files > /dev/null 2>&1)
-					if [ $? -ne 0 ]; then 
-						echo "$entity" >> "$added_files"
+		# Evitam linii de tipul "+++ *" sau "--- *"
+		if [[ $line != +++' '* && $line != ---' '* ]]; then
+			revline="$(echo "$line" | rev)"
+			mountpoint="$(echo "$revline" | cut -d" " -f1 | rev)"
+			if [[ $mountpoint == '/'* ]]; then
+				found=0
+				while read -r checked_line; do
+					if [[ $checked_line == $mountpoint ]]; then
+						found=1
 					fi
-					;;
-					'd')
-					exists=$(grep "$entity" $modified_dirs > /dev/null 2>&1)
-					if [ $? -ne 0 ]; then 
-						echo "$entity" >> "$added_dirs"
-					fi
-					;;
-				esac
-				;;
-				'-')
-				case $char1 in
-					'-')
-					exists=$(grep "$entity" $modified_files > /dev/null 2>&1)
-					if [ $? -ne 0 ]; then 
-						echo "$entity" >> "$removed_files"
-					fi
-					;;
-					'd')
-					exists=$(grep "$entity" $modified_dirs > /dev/null 2>&1)
-					if [ $? -ne 0 ]; then 
-						echo "$entity" >> "$removed_dirs"
-					fi
-					;;
-				esac
-				;;
-			esac
+				done < "$modified_mountpoints" > /dev/null 2>&1
+				if [ $found -eq 0 ]; then 
+					case $char0 in
+						+)
+						echo $mountpoint >> "$added_mountpoints"
+						;;
+						-)
+						echo $mountpoint >> "$removed_mountpoints"
+						;;
+					esac
+				fi
+			fi
 		fi
-	done < "$df_diff"
+	done < "$df_diff" > /dev/null 2>&1
 	
+	echo -e "${LCYAN}Spatiu pe disc:${NC}"
+
+	cat "$modified_size_used"> /dev/null 2>&1
+	if [ $? -eq 0 ]; then
+		echo "Mountpoint-uri unde s-a modificat spatiul pe disc:"
+		cat "$modified_size_used"
+		echo ""
+		anyDfChanges=1
+	fi
+
+	cat "$added_mountpoints" > /dev/null 2>&1
+	if [ $? -eq 0 ]; then
+		echo "Mountpoint-uri noi:"
+		cat "$added_mountpoints"
+		echo ""
+		anyDfChanges=1
+	fi
+
+	cat "$removed_mountpoints" > /dev/null 2>&1
+	if [ $? -eq 0 ]; then
+		echo "Mountpoint-uri sterse:"
+		cat "$removed_mountpoints"
+		echo ""
+		anyDfChanges=1
+	fi
+
+	if [[ $anyDfChanges == 0 ]]; then
+		echo -e "${GREEN}Structura nemodificata!${NC}\n"
+	fi
 
 	read -p "Apasa [ENTER] pentru a continua"
+	clear
+
+	# Dam utilizatorului optiunea sa faca diff pe fisierele gasite modificate, daca exista asa ceva
+	cat "$modified_files" > /dev/null 2>&1
+	if [ $? -eq 0 ]; then
+		modified_files_cleaned="${modified_files}_cleaned"
+		tr -d $'\r' < "$modified_files" > "$modified_files_cleaned"
+		returnToMain=0
+		while true; do
+			if [ $returnToMain -eq 1 ]; then
+				clear && break
+			fi
+			read -p "Doresti sa compari si continutul fisierelor modificate? [DA/NU]: " ID
+			clear
+			case $ID in
+				"DA")
+				while true; do
+					echo -e "Fisiere modificate disponibile:${YELLOW}"
+					cat $modified_files_cleaned
+					echo -e "\n${NC}Pentru a iesi in meniul principal, introduceti textul ${YELLOW}MAIN MENU${NC}."
+					read -p "Alegeti un fisier: " FNAME
+					if [[ "$FNAME" == "MAIN MENU" ]]; then
+						returnToMain=1
+						rm -rf "$TEMPDIR" ; rm -rf "$TSPDIR1" ; rm -rf "$TSPDIR2"
+						break
+					fi
+					found=0
+					#echo $FNAME
+					while read -r line; do
+						#echo $line
+						if [[ $line == $FNAME ]]; then
+							found=1
+							#echo "FOUND!!!"
+						fi
+					done < "$modified_files_cleaned"
+
+					clear
+					#exists=$(/bin/grep "$(echo "$FNAME" | tr -d $'\r')" "$modifies_files_cleaned")
+					case $found in
+						1)
+						diff_files "$FNAME"
+						;;
+						*)
+						echo -e "${RED}Optiune invalida${NC}"
+						;;
+					esac
+				done
+				;;
+				"NU")
+				break
+				;;
+				*)
+				clear
+				echo -e "${RED}Optiune invalida${NC}"
+				;;
+			esac
+		done 
+	fi
+
 	rm -rf "$TEMPDIR" ; rm -rf "$TSPDIR1" ; rm -rf "$TSPDIR2"
 	if [[ "$2" == "tempLiveSnapshot" ]]; then
 		rm -f "$TS2"
@@ -403,9 +511,21 @@ compare() {
 	clear 
 }
 
+diff_files () {
+	FNAME="$1"
+	echo -e "${LCYAN}======== FISIER MODIFICAT  ========\n${NC}$USERDIR/$FNAME\n"
+	diff -u --color $TSEFD1/$FNAME $TSEFD2/$FNAME
+	echo ""
+	read -p "Apasa [ENTER] pentru a continua."
+	clear
+}
+
 # START
 clear
 while true; do
+	rm -rf "$TEMPDIR"
+	present="$TSDIR/tempLiveSnapshot"
+	rm -rf "$present" "${present}_PARSED"
 	echo -e "${LCYAN}============== FDUM ==============\n${NC}FIle&DiskUsageMonitor - Main Menu\n"
 	echo "1. Genereaza un nou snapshot (typescript)"
 	echo "2. Compara doua snapshot-uri"
@@ -414,31 +534,89 @@ while true; do
 	read -p "Selectati optiunea: " ID
 	case $ID in
 		1)
-		echo ""
-		read -p "Denumirea noului snapshot (typescript): " TSNAME
-		generate_typescript "$TSNAME"
+		clear
+		while true; do
+			echo -e "${LCYAN}========= GENEREAZA SNAPSHOT =========${NC}"
+			echo -e "Pentru a iesi in meniul principal, introduceti [${YELLOW}0${NC}]"			
+			read -p "Denumirea noului snapshot (typescript): " TSNAME
+			case "$TSNAME" in
+				0)
+				# Intoarcere la main menu
+				clear
+				break
+				;;
+				""|*[![:alnum:]]*)
+				clear
+				echo -e "${RED}Optiune invalida!"
+				echo -e "Numele typescript-ului trebuie sa contina doar caractere alfanumerice${NC}"
+				;;
+				*)
+				generate_typescript "$TSNAME"
+				break;
+				;;
+			esac
+		done
 		;;
 		2)
 		clear
-		echo -e "Snapshot-uri disponibile:${YELLOW}"
-		/bin/ls -p "$TSDIR" | grep -v /
-		echo -e "${NC}"
-		read -p "Numele primului snapshot: " TS1
-		read -p "Numele celui de-al doilea snapshot: " TS2
-		clear
-		compare "$TS1" "$TS2" 2> /dev/null
+		while true; do
+			echo -e "${LCYAN}=============== COMPARA ===============${NC}"
+			echo -e "Snapshot-uri disponibile:${YELLOW}"
+			/bin/ls -p "$TSDIR" | grep -v /
+			echo -e "\n${NC}Pentru a iesi in meniul principal, introduceti [${YELLOW}0${NC}] in oricare camp."		
+			read -p "Numele primului snapshot: " TS1
+			if [[ $TS1 == 0 ]]; then
+				clear && break
+			fi
+			read -p "Numele celui de-al doilea snapshot: " TS2
+			if [[ $TS2 == 0 ]]; then
+				clear && break
+			fi
+			/bin/ls -p "$TSDIR" | grep -v / | grep -w $TS1 > /dev/null 2>&1 && /bin/ls -p "$TSDIR" | grep -v / | grep -w $TS2 > /dev/null 2>&1
+			case $? in
+				0)
+				clear
+				compare "$TS1" "$TS2"
+				break
+				;;
+				*)
+				clear
+				echo -e "${RED}Eroare!"
+				echo -e "Numele snapshot-urilor trebuie sa corespunda cu cele disponibile.${NC}"
+				;;
+			esac
+		done
 		;;
 		3)
 		clear
-		echo -e "Snapshot-uri disponibile:${YELLOW}"
-		/bin/ls -p "$TSDIR" | grep -v /
-		echo -e "${NC}"
-		read -p "Numele snapshot-ului: " TS1
-		TS2="tempLiveSnapshot"
-		generate_typescript "$TS2"
-		compare "$TS1" "$TS2" 2> /dev/null
-		rm -f "$TSDIR/$TS2"
+		while true; do
+			echo -e "${LCYAN}========= COMPARA CU PREZENTUL =========${NC}"
+			echo -e "Snapshot-uri disponibile:${YELLOW}"
+			/bin/ls -p "$TSDIR" | grep -v /
+			echo -e "\n${NC}Pentru a iesi in meniul principal, introduceti [${YELLOW}0${NC}]."		
+			read -p "Alege snapshot-ul: " TS1
+			if [[ $TS1 == 0 ]]; then
+				clear && break
+			fi
+			TS2="tempLiveSnapshot"
+			/bin/ls -p "$TSDIR" | grep -v / | grep -w $TS1 > /dev/null 2>&1
+			case $? in
+				0)
+				clear
+				generate_typescript "$TS2" > /dev/null
+				clear
+				compare "$TS1" "$TS2"
+				break
+				;;
+				*)
+				clear
+				echo -e "${RED}Eroare!"
+				echo -e "Numele snapshot-urilor trebuie sa corespunda cu cele disponibile.${NC}"
+				;;
+			esac
+		done
 		;;
+		
 		4)
 		clear
 		exit 0
